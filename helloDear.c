1ea42293d5fbe73979ab08531da946dbbcbe87ef
AnilGadamsetty <anilgk81@gmail.com>
2022-07-24 15:27:36 -0700

    modified helloDear file

--- helloDear.c
+++ helloDear.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello this is my program in vim editor");
+	printf("Hello this is my program in vim editor\n");
+	int a = 22, b = 20;
+	printf("Total age is %d", a+b);
 
 	return 0;
 }
